704afcfbfc8fce02bcea409ff05748f15ed29910
sunbreak-pro <fstprog@gmail.com>
2024-08-30 08:41:55 +0900

    カリキュラム07-1Postコントローラーへのルーティング設定まで

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
+Route::get('/posts', [PostController::class, 'index']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+?>
